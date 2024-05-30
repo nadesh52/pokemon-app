5b223d28e707d27a1e99d283b9a1e11275efcb07
nadesh <worldaroundmee@gmail.com>
2024-05-31 04:00:49 +0700

    update page

--- app/pokemon/[pokemonId]/page.tsx
+++ app/pokemon/[pokemonId]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import Image from "next/image";
-import React, { useEffect, useState } from "react";
+// import Image from "next/image";
+import React from "react";
 
 const PokemonPage = ({ params }: { params: { pokemonId: string } }) => {
   // const [pokemon, setPokemon] = useState({ sprites: { front_default: "" } });
